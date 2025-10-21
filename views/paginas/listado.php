1b529beedbc274430e2326690f76fcfd946d4417
David Cano Marin <davidcanomarin23@gmail.com>
2025-10-21 16:35:39 -0500

    Actualizacion 21/10/2025 - 4:35 PM

--- views/paginas/listado.php
+++ views/paginas/listado.php
@@ -1,5 +1,5 @@
 <?php foreach($propiedades as $propiedad){ ?>
-
+    <!-- <a href="/propiedad?id=<?php echo $propiedad->id; ?>&tipo=<?php echo strtolower($propiedad->tipo); ?>" class="boton boton-amarillo-block"> -->
         <div class="card-propiedad">
             <div class="swiper img-container">
                 <div class="swiper-wrapper">
@@ -23,16 +23,42 @@
                 <div class="swiper-button-next"></div>
             </div>
 
+            <div class="contenedor_padre_computador_informacion_card">
             <h3>$<?php echo number_format((int)str_replace('.', '', $propiedad->{'precio'}), 0, ',', '.'); ?> COP</h3>
 
 
         <?php if ($propiedad->{'tipo'} === 'Casa' || $propiedad->{'tipo'} === 'Finca' || $propiedad->{'tipo'} === 'Apartamento' || $propiedad->{'tipo'} === 'Apartaestudio' || $propiedad->{'tipo'} === 'Apartaoficina') : ?>  
             <div class="carac">
-                <p>Baños: <?php echo $propiedad->{'banos'};?></p>
-                <p>Habitaciones: <?php echo $propiedad->{'habitaciones'};?></p>
-                <?php if($propiedad->estrato != 0): ?>
-                <p>Estrato: <?php echo $propiedad->{'estrato'};?></p>
+                <?php if($propiedad->{'banos'} == 1): ?>
+                <div class="contenedor_caracteristicas">
+                    <img src="/img/inodoro.png" alt="">
+                    <p><?php echo $propiedad->{'banos'};?> Baño</p>
+                </div>
+                <?php else: ?>
+                    <div class="contenedor_caracteristicas">
+                    <img src="/img/inodoro.png" alt="">
+                    <p><?php echo $propiedad->{'banos'};?> Baños</p>
+                </div>
                 <?php endif; ?>
+
+                <div class="contenedor_caracteristicas">
+                    <img src="/img/dormitorio.png" alt="">
+                    <p><?php echo $propiedad->{'habitaciones'};?> Habs</p>
+                </div>
+
+                <?php if($propiedad->estrato != 0): ?>
+                <div class="contenedor_caracteristicas">
+                    <img src="/img/estrato.png" alt="">
+                <p><?php echo $propiedad->{'estrato'};?> Estrato</p>
+                </div>
+                <?php endif; ?>
+
+                <?php if (!empty($propiedad->area_total) || $propiedad->area_total != 0) { ?>
+                    <div class="contenedor_caracteristicas">
+                        <img src="/img/area.png" alt="">
+                        <p><?php echo $propiedad->{'area_total'}; ?>m²</p>
+                    </div>
+                <?php } ?>
             </div>
         <?php endif; ?>
 
@@ -56,11 +82,12 @@
                 
 
                 <?php if (!empty($propiedad->area_total) || $propiedad->area_total != 0) { ?>
-                    <p>Area: <?php echo $propiedad->{'area_total'}; ?>m²</p>
+                    <p class="area_movil">Area: <?php echo $propiedad->{'area_total'}; ?>m²</p>
                 <?php } ?>
 
 
-                <p>Tipo de Propiedad: <?php echo $propiedad->{'tipo'};?></p>
+                <p class="texto_movil_informacion_tipo_ubicacion_barrio">Tipo de Propiedad: <?php echo $propiedad->{'tipo'};?></p>
+                <p class="texto_computador_informacion_tipo_ubicacion_barrio"> <?php echo $propiedad->{'tipo'} . ' en ' . $propiedad->{'barrio'} . ', ' . $propiedad->{'ubicacion'}; ?> </p>
             </div>
 
             <div class="linea-blanca"></div>
@@ -72,9 +99,10 @@
                 <?php endif; ?>
             </div>
 
-            <a href="/propiedad?id=<?php echo $propiedad->id; ?>&tipo=<?php echo strtolower($propiedad->tipo); ?>" class="boton boton-amarillo-block">
-    Ver propiedad
-</a>
+        </div>
+            
+                
+            <!-- </a> -->
 
         </div>
 
