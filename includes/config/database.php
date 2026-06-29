c084c5ab29fbbcce4773c8ba8bbb6ecd0e1ce3b1
davidcano23 <124854345+davidcano23@users.noreply.github.com>
2026-06-28 23:34:17 -0500

    1.01

--- includes/config/database.php
+++ includes/config/database.php
@@ -3,9 +3,9 @@
 function conectarDB() : mysqli {
     $db = new mysqli(
         'localhost',          // host — en Hostinger casi siempre es 'localhost'
-        'TU_USUARIO_DB',      // usuario de la base de datos (panel Hostinger)
-        'TU_PASSWORD_DB',     // contraseña
-        'TU_NOMBRE_DB'        // nombre de la base de datos
+        'root',      // usuario de la base de datos (panel Hostinger)
+        'root',     // contraseña
+        'zubanabienraiz'        // nombre de la base de datos
     );
 
     if(!$db) {
